423e6dc2134b72d5e377b031e2679c770d74a2c6
adperem <adperem@unirioja.es>
2024-05-01 20:49:08 +0200

    NutAngles

---
+++ include/NutAngles.h
@@ -0,0 +1,25 @@
+//
+// Created by perez on 01/05/2024.
+//
+
+#ifndef PROYECTOTALLER1_NUTANGLES_H
+#define PROYECTOTALLER1_NUTANGLES_H
+#include "SAT_Const.h"
+#include "Matrix.h"
+//--------------------------------------------------------------------------
+//
+// NutAngles.m
+//
+// Purpose:
+//   Nutation in longitude and obliquity
+//
+// Input:
+//   Mjd_TT     Modified Julian Date (Terrestrial Time)
+//
+// Output:
+//   dpsi,deps  Nutation Angles
+//
+// Last modified:   2015/08/12   M. Mahooti
+//
+//--------------------------------------------------------------------------
+#endif //PROYECTOTALLER1_NUTANGLES_H
