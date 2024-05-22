0f4f1049c352fe4dcca0cf906da5d1b501c37298
adperem <adperem@unirioja.es>
2024-05-22 21:00:19 +0200

    testNutAngles

--- test/testNutAngles.cpp
+++ test/testNutAngles.cpp
@@ -6,13 +6,15 @@
 #include <iostream>
 #include <cmath>
 
+
+
 int main() {
     double deps, dpsi; // Declara las variables antes de usarlas
-    NutAngles(2459246, dpsi, deps); // Llama a la función NutAngles
+    NutAngles(4.974611085861109e+04, dpsi, deps); // Llama a la función NutAngles
 
     // Realiza la aserción
-    assert(fabs(dpsi)- 8.23714216989103e-05 < 1e-10);
-    assert(fabs(deps -  9.14097460043146e-06) < 1e-10);
+    assert(fabs(dpsi- 6.230692866336197e-05 < 1e-15));
+    assert(fabs(deps + 3.511084590138603e-05) < 1e-15);
 
     std::cout<< "Test pasados con éxito"<< std::endl;
 
