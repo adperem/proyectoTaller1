ef53e981491a08bd79f09d4775bb1dd72316442f
adperem <adperem@unirioja.es>
2024-05-22 22:47:43 +0200

    .

--- src/elements.cpp
+++ src/elements.cpp
@@ -1,27 +1,31 @@
 //
 // Created by perez on 02/05/2024.
 //
+#include <cstdio>
 #include "elements.h"
 
 void elements(Matrix y, double& p, double& a, double& e, double& i, double& Omega, double& omega, double& M) {
-    double pi2 = 2 * M_PI;
+    double pi2 = 2.0 * M_PI;
 
     Matrix r(1, 3);             // Position
     r(1, 1) = y(1, 1);
-    r(1, 2) = y(1, 2);
-    r(1, 3) = y(1, 3);
+    r(1, 2) = y(2, 1);
+    r(1, 3) = y(3, 1);
 
     Matrix v(1, 3);              // Velocity
-    v(1, 1) = y(1, 4);
-    v(1, 2) = y(1, 5);
-    v(1, 3) = y(1, 6);
+    v(1, 1) = y(1, 2);
+    v(1, 2) = y(2, 2);
+    v(1, 3) = y(3, 2);
 
-    Matrix h = cross(r, v);                                    // Areal velocity
+    Matrix h = cross(r, v);
+    // Areal velocity
     double magh = norm(h);
-    p = magh * magh /SAT_Const::GM_Earth;
+    p = magh * magh /(SAT_Const::GM_Earth);
     double H = norm(h);
 
-    Omega = atan2 ( h(1,1), -h(1,2) );                     // Long. ascend. node
+    Omega = atan2 ( h(1,1), -h(1,2) );
+    // Long. ascend. node
+    printf("%5.20lf", fmod(Omega, pi2));
     Omega = fmod(Omega, pi2);
     i     = atan2 ( sqrt(h(1,1)*h(1,1)+h(1,2)*h(1,2)), h(1,3) ); // Inclination
     double u= atan2 ( r(1,3)*H, -r(1,1)*h(1,2)+r(1,2)*h(1,1) );    // Arg. of latitude
@@ -29,10 +33,10 @@ void elements(Matrix y, double& p, double& a, double& e, double& i, double& Omeg
 
     double R  = norm(r);                                      // Distance
 
-    a = 1/(2/R-dot(v,v)/SAT_Const::GM_Earth);               // Semi-major axis
+    a = 1.0/(2.0/R-dot(v,v)/(SAT_Const::GM_Earth));               // Semi-major axis
 
-    double eCosE = 1-R/a;                                     // e*cos(E)
-    double eSinE = dot(r,v)/sqrt(SAT_Const::GM_Earth*a);           // e*sin(E)
+    double eCosE = 1.0-R/a;                                     // e*cos(E)
+    double eSinE = dot(r,v)/sqrt((SAT_Const::GM_Earth)*a);           // e*sin(E)
 
     double e2 = eCosE*eCosE +eSinE*eSinE;
     e  = sqrt(e2);                                     // Eccentricity
--- test/testElements.cpp
+++ test/testElements.cpp
@@ -7,20 +7,26 @@
 #include "elements.h"
 
 int main() {
-    Matrix m1(1, 6);
-    for (int i = 1; i <= 6; ++i) {
-        m1(1, i) = i;
-    }
-    double p, a, e, i, Omega, omega, M;
-    elements(m1, p, a, e, i, Omega, omega, M);
+    Matrix y(3, 2);
+    y(1, 1) = 6221397.62857869;
+    y(1, 2) = 4645.04725161806;
+    y(2, 1) = 2867713.77965738;
+    y(2, 2) = -2752.21591588204;
+    y(3, 1) = 3006155.98509949;
+    y(3, 2) = -7507.99940987031;
 
-    assert(fabs(p - ) < 1e-10);
-    assert(fabs(a - ) < 1e-10);
-    assert(fabs(e - ) < 1e-10);
-    assert(fabs(i - ) < 1e-10);
-    assert(fabs(Omega - ) < 1e-10);
-    assert(fabs(omega - ) < 1e-10);
-    assert(fabs(M - ) < 1e-10);
+
+    double p, a, e, i, Omega, omega, M;
+    elements(y, p, a, e, i, Omega, omega, M);
+
+    printf("%5.20lf", fabs(Omega - 3.356710766508286));
+    assert(fabs(p - 1.200169359721398e+07) < 1e-7);
+    assert(fabs(a - 1.894392266071454e+07) < 1e-7);
+    assert(fabs(e - 0.605361104987026) < 1e-7);
+    assert(fabs(i - 2.026562955350169) < 1e-7);
+    assert(fabs(Omega - 3.356710766508286) < 1e-7);
+    assert(fabs(omega - 2.737572897725617) < 1e-7);
+    assert(fabs(M - 6.271446933419666) < 1e-7);
 
     std::cout << "Tests pasados con éxito" << std::endl;
     return 0;
